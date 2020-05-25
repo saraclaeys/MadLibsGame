1bdc259a9f22d8d5c54bcfdbd6497fef124f2280
saraclaeys <sara.claeys01@gmail.com>
2020-05-25 15:51:30 +0200

    initial commit

---
+++ MadLibsGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MadLibsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
