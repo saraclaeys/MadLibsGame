cae1be0bbfc3881fa975f1385afae321e31fd2e2
saraclaeys <sara.claeys01@gmail.com>
2020-05-25 15:59:33 +0200

    simple mad libs game

--- MadLibsGame/Program.cs
+++ MadLibsGame/Program.cs
@@ -6,7 +6,23 @@ namespace MadLibsGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
         }
     }
 }
